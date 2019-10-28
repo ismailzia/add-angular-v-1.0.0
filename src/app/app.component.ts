959545aed1315855d7459c46a1bb87c763213cfa
ziani ismail <ziani0722@gmail.com>
2019-10-28 12:47:17 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appAdd';
+
+  newArr = [];
+  serverName:'';
+  contetn:''
+
+
+  addServer(){
+   this.newArr.push({
+     type:'server',
+     name:this.serverName,
+     text:this.contetn
+   })
+   this.serverName='';
+   this.contetn=''
+  }
+  addServerDanger(){
+    this.newArr.push({
+      type:'serverRed',
+      name:this.serverName,
+      text:this.contetn
+    })
+    this.serverName='';
+    this.contetn=''
+  }
+
 }
